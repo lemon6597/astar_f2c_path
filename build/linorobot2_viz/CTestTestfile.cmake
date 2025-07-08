5c977b8a5678a654cd9126f5b647766220302951
lemon6597 <kameliy6597@gmail.com>
2025-07-08 20:11:23 +0800

    add linorobot2

---
+++ build/linorobot2_viz/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/humble/ros2_ws/src/linorobot2_viz
+# Build directory: /home/humble/ros2_ws/build/linorobot2_viz
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
--- install/py_pub_sub/share/py_pub_sub/launch/plan_launch.py
+++ install/py_pub_sub/share/py_pub_sub/launch/plan_launch.py
@@ -8,13 +8,20 @@ def generate_launch_description():
             executable='Astar',
             name='Astar'
         )
+    Field2Cover_node = Node(
+            package='py_pub_sub',
+            executable='Field2Cover',
+            name='Field2Cover'
+    )
     Start_node = Node(
             package='py_pub_sub',
             executable='Start',
             name='Start'
         )
     
+    
     return LaunchDescription([
         Start_node,
-        Astart_node
+        Astart_node,
+        Field2Cover_node
     ])
