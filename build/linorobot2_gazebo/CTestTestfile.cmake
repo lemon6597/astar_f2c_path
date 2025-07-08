d69ebecee1718c220b4979a46ea4a390d0057488
lemon6597 <kameliy6597@gmail.com>
2025-07-08 23:19:20 +0800

    myrepo

--- build/linorobot2_gazebo/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/humble/ros2_ws/src/linorobot2/linorobot2_gazebo
-# Build directory: /home/humble/ros2_ws/build/linorobot2_gazebo
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
